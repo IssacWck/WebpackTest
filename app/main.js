20a93dd01cdc4ddb06de8d6a674256236821db88
IssacWck <kailovelee@163.com>
2016-11-24 11:21:52 +0800

    delete

--- app/main.js
+++
@@ -1,9 +0,0 @@
-//var greeter = require('./greeter.js');
-//document.getElementById('root').appendChild(greeter());
-import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './greeter'
-
-import './main.css';
-
-render(<Greeter />, document.getElementById('root'));
